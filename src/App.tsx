f42ae9872a9fcd40bc05506f4bf2318bafc847b5
Teeraphon Sudkane <teeraphonx@gmail.com>
2025-11-26 00:45:00 +0700

    App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import ProductDetail from "./pages/ProductDetail";
 import NotFound from "./pages/NotFound";
@@ -10,7 +11,7 @@ import Aboutus from "./pages/Aboutus";
 import ContactUs from "./pages/ContactUs";
 import Howtobuy from "./pages/Howtobuy";
 import Faq from "./pages/Faq";
-import ProductCategories from "@/pages/ProductCategories";
+import ProductCategories from "./pages/ProductCategories";
 
 const queryClient = new QueryClient();
 
@@ -21,18 +22,22 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+
           <Route path="/" element={<Index />} />
-          <Route path="/product/:handle" element={<ProductDetail />} />
-          {/* <Route path="/products/mouse" element={<Mouse />} />
-          <Route path="/products/mouse-feet" element={<MouseFeet />} />
-          <Route path="/products/mousepads" element={<MousePads />} />
-          <Route path="/products/mouse-grips" element={<MouseGrips />} /> */}
+          <Route path="/product/:id" element={<ProductDetail />} />
+
+          {/* ROUTES หมวดสินค้า */}
           <Route path="/products/:categoryId" element={<ProductCategories />} />
-          <Route path="*" element={<NotFound />} />
+
+          {/* หน้าทั่วไป */}
           <Route path="/Aboutus" element={<Aboutus />} />
           <Route path="/ContactUs" element={<ContactUs />} />
           <Route path="/Howtobuysection" element={<Howtobuy />} />
           <Route path="/faq" element={<Faq />} />
+
+          {/* 404 */}
+          <Route path="*" element={<NotFound />} />
+
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
